f5d5d65bd2498e1c8638e5c9f7112d8d294a631a
iaio998 <depalatisiacopo@gmail.com>
2023-11-09 14:40:54 +0100

    First server attempt error

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,20 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+import App1Header from './components/App1Header.vue'
+// import TheWelcome from './components/TheWelcome.vue'
+
+export default {
+  name: 'App',
+  components:{
+    App1Header,
+
+  },
+  data(){
+    return{
+
+    }
+  },
+  methods:{}
+}
 </script>
 
 <template>
---
+++ src/components/App1Header.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+<div class="container">
+    <img src="" alt="">
+    <ul>
+       <li><a href="#">1</a></li>
+       <li><a href="#">2</a></li>
+       <li><a href="#">3</a></li>
+    </ul>
+</div>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name:"App1Header",
+        data(){
+            return{
+
+            }
+        },
+        methods:{},
+    }
+</script>
+
+<style scoped>
+
+@use "../assets/main.css";
+
+</style>
